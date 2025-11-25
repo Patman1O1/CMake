c61674ad8577164f3562ac7610bd6373e3e55735
Patman1O1 <patrickthomas101x@gmail.com>
2025-11-25 15:17:03 -0600

    Add variables

---
+++ variables/variables.cmake
@@ -0,0 +1,6 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# CMake
+#-----------------------------------------------------------------------------------------------------------------------
+set(CMAKE_MINIMUM_REQUIRED_VERSION 3.5 CACHE INTERNAL "CMAKE_MINIMUM_REQUIRED_VERSION")
+set(CMAKE_VERSION 4.1 CACHE INTERNAL "CMAKE_VERSION")
+set(CMAKE_CXX_STANDARD )
