d8d8e9c6b9cd4c85dfffc053b47e92a8ae70febf
Patman1O1 <patrickthomas101x@gmail.com>
2025-11-25 15:20:09 -0600

    Remove variables/variables.cmake

--- variables/variables.cmake
+++
@@ -1,6 +0,0 @@
-#-----------------------------------------------------------------------------------------------------------------------
-# CMake
-#-----------------------------------------------------------------------------------------------------------------------
-set(CMAKE_MINIMUM_REQUIRED_VERSION 3.5 CACHE INTERNAL "CMAKE_MINIMUM_REQUIRED_VERSION")
-set(CMAKE_VERSION 4.1 CACHE INTERNAL "CMAKE_VERSION")
-set(CMAKE_CXX_STANDARD )
