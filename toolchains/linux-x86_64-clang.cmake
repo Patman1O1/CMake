22de308b09070949089069ab6b9e34dd318e5963
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-09 22:04:23 -0600

    Edit linux-x86_64-clang.cmake

--- toolchains/linux-x86_64-clang.cmake
+++ toolchains/linux-x86_64-clang.cmake
@@ -3,9 +3,11 @@
 # -------------------------------------------------
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
 find_program(CMAKE_C_COMPILER "clang" REQUIRED)
 find_program(CMAKE_CXX_COMPILER "clang++" REQUIRED)
 find_program(LLD lld)
+
 if(LLD)
     set(CMAKE_EXE_LINKER_FLAGS_INIT    "-fuse-ld=lld")
     set(CMAKE_SHARED_LINKER_FLAGS_INIT "-fuse-ld=lld")
