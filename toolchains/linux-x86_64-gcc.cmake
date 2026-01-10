48fe5588564c655a1325eff2d89663172edaf9ba
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-09 22:04:53 -0600

    Edit linux-x86_64-gcc.cmake

--- toolchains/linux-x86_64-gcc.cmake
+++ toolchains/linux-x86_64-gcc.cmake
@@ -3,5 +3,6 @@
 # -------------------------------------------------
 set(CMAKE_SYSTEM_NAME "Linux")
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
+
 find_program(CMAKE_C_COMPILER "gcc" REQUIRED)
 find_program(CMAKE_CXX_COMPILER "g++" REQUIRED)
