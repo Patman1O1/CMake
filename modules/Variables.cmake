409dc9c570839ef45b4299769b4abb44d5952eaa
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-10 15:56:37 -0600

    Add Variables.cmake

---
+++ modules/Variables.cmake
@@ -0,0 +1,19 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# CMake Variables
+#-----------------------------------------------------------------------------------------------------------------------
+set(CMAKE_VERSION 4.2 CACHE STRING "CMAKE_VERSION")
+set(CMAKE_MINIMUM_REQUIRED_VERSION 4.2 CACHE STRING "CMAKE_MINIMUM_REQUIRED_VERSION")
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# C Variables
+#-----------------------------------------------------------------------------------------------------------------------
+set(CMAKE_C_STANDARD 23 CACHE STRING "CMAKE_C_STANDARD")
+set(CMAKE_C_STANDARD_REQUIRED ON CACHE STRING "CMAKE_C_STANDARD_REQUIRED")
+
+
+#-----------------------------------------------------------------------------------------------------------------------
+# C++ Variables
+#-----------------------------------------------------------------------------------------------------------------------
+set(CMAKE_CXX_STANDARD 23 CACHE STRING "CMAKE_CXX_STANDARD")
+set(CMAKE_CXX_STANDARD_REQUIRED ON CACHE STRING "CMAKE_CXX_STANDARD_REQUIRED")
