1345058afe349ef780f80fbc580dd516aff359b1
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-16 00:58:25 -0600

    Edit uninstall-cmake.sh

--- uninstall-cmake.sh
+++ uninstall-cmake.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-sudo rm -rf "/opt/cmake"
+# Check if CMake is already uninstalled
+if [ ! -e "/opt/cmake" && ! -e "/etc/profile.d/cmake-bin-path.sh"]; then
+    echo "CMake is already uninstalled"
+    exit 0
+fi
 
+# Uninstall CMake
+sudo rm -rf "/opt/cmake"
+sudo rm -f "/etc/profile.d/cmake-bin-path.sh"
+
+# Let the user know that CMake was successfully uninstalled
 echo "Successfully uninstalled CMake"
